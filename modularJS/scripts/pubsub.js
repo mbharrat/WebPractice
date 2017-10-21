bb4ea1357ee181927c3e4c60943ef3be76481612
mbharrat <mbharrat@gmail.com>
2017-10-21 00:31:35 -0400

    added modularjs examples

---
+++ modularJS/scripts/pubsub.js
@@ -0,0 +1,26 @@
+//entity that makes sync up of multiple modules easy
+//not currently used in modularJS project
+var events = {
+  events: {},
+  on: function (eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+  off: function(eventName, fn) {
+    if (this.events[eventName]) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      };
+    }
+  },
+  emit: function (eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  }
+};
